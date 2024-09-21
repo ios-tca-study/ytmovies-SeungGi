bdef95703832cb231a5f827af3c3619e06fa4c3e
Wallaby <avocado34.131@gmail.com>
2024-09-21 15:48:18 +0900

    [Feat] 메인 탭 구현

--- YSTMovie TCA/YSTMovie TCA/Sources/View/MainTabView.swift
+++ YSTMovie TCA/YSTMovie TCA/Sources/View/MainTabView.swift
@@ -10,16 +10,66 @@ import SwiftUI
 struct MainTabView: View {
   
   // MARK: - Properties
+  @State private var selectedTab: Tab = .home
   
-  // MARK: - Initializers
+  enum Tab {
+    case home
+    case search
+    case bookmark
+  }
   
   // MARK: - Views
-  
   var body: some View {
     NavigationStack {
-      HomeView()
+      ZStack {
+        // Views for each tab
+        switch selectedTab {
+        case .home:
+          HomeView()
+        case .search:
+          SearchView()
+        case .bookmark:
+          BookmarkView()
+        }
+        
+        VStack {
+          Spacer()
+          HStack {
+            Spacer()
+            
+            TabButton(tab: .home, selectedTab: $selectedTab, iconName: "home")
+            Spacer()
+            TabButton(tab: .search, selectedTab: $selectedTab, iconName: "search")
+            Spacer()
+            TabButton(tab: .bookmark, selectedTab: $selectedTab, iconName: "bookmark")
+            
+            Spacer()
+          }
+          .frame(height: 70)
+          .background(.black)
+        }
+      }
     }
-    .toolbarVisibility(.hidden, for: .navigationBar)
+    .toolbar(.hidden, for: .navigationBar)
+  }
+}
+
+// Tab Button
+struct TabButton: View {
+  let tab: MainTabView.Tab
+  @Binding var selectedTab: MainTabView.Tab
+  let iconName: String
+  
+  var body: some View {
+    Button(action: {
+      selectedTab = tab
+    }) {
+      Image(iconName)
+        .resizable()
+        .scaledToFit()
+        .frame(height: 20)
+        .foregroundColor(selectedTab == tab ? .accentColor : .white)
+    }
   }
 }
 
