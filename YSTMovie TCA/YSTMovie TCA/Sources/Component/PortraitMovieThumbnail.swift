3fa75d2ea15af81471eb96c7a9e6e55a325ee3c6
Wallaby <avocado34.131@gmail.com>
2024-09-21 14:48:32 +0900

    [Feat] PortraitMovieThumbailView 추가

---
+++ YSTMovie TCA/YSTMovie TCA/Sources/Component/PortraitMovieThumbnail.swift
@@ -0,0 +1,18 @@
+//
+//  PortraitMovieThumbnail.swift
+//  YSTMovie TCA
+//
+//  Created by 이승기 on 9/21/24.
+//
+
+import SwiftUI
+
+struct PortraitMovieThumbnail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortraitMovieThumbnail()
+}
