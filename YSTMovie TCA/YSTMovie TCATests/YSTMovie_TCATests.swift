b8de25dea0edfe5cf4b024233870f7282e38aa51
Wallaby <avocado34.131@gmail.com>
2024-09-21 14:14:59 +0900

    Initial setting

---
+++ YSTMovie TCA/YSTMovie TCATests/YSTMovie_TCATests.swift
@@ -0,0 +1,17 @@
+//
+//  YSTMovie_TCATests.swift
+//  YSTMovie TCATests
+//
+//  Created by 이승기 on 9/21/24.
+//
+
+import Testing
+@testable import YSTMovie_TCA
+
+struct YSTMovie_TCATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
