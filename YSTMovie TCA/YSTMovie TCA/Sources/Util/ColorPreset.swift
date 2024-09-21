444fa965943b085d44f24c3172babccb868eb396
Wallaby <avocado34.131@gmail.com>
2024-09-21 14:41:12 +0900

    [Feat] 프리셋 추가

---
+++ YSTMovie TCA/YSTMovie TCA/Sources/Util/ColorPreset.swift
@@ -0,0 +1,15 @@
+//
+//  ColorPreset.swift
+//  YSTMovie TCA
+//
+//  Created by 이승기 on 9/21/24.
+//
+
+import SwiftUI
+
+extension Color {
+  static let ytGray10 = Color("gray10")
+  static let ytGray30 = Color("gray30")
+  static let ytGray50 = Color("gray50")
+  static let ytGray70 = Color("gray70")
+}
---
+++ YSTMovie TCA/YSTMovie TCA/Sources/Util/ImagePreset.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePreset.swift
+//  YSTMovie TCA
+//
+//  Created by 이승기 on 9/21/24.
+//
+
+import SwiftUI
+
+extension Image {
+  static let bookmark_fill = Image("bookmark-fill")
+  static let bookmark = Image("bookmark")
+  static let home = Image("home")
+  static let search = Image("search")
+  static let star_empty = Image("star-empty")
+  static let star_fill = Image("star-fill")
+  static let star_half = Image("star-half")
+}
