54d35299048ddaac0fdd9927c169392fc2ef5e7a
Wallaby <avocado34.131@gmail.com>
2024-10-09 21:09:53 +0900

    [Feat] SearchMovieUseCase 추가

---
+++ YSTMovie TCA/YSTMovie TCA/Sources/Domain/UseCase/SearchMovieUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  SearchMovieUseCase.swift
+//  YSTMovie TCA
+//
+//  Created by 이승기 on 10/9/24.
+//
+
+import Foundation
+
+struct SearchMovieUseCase {
+  
+  // MARK: - Properties
+  
+  private let repository: YTSMovieRepository
+  
+  
+  // MARK: - Initializers
+  
+  init(repository: YTSMovieRepository) {
+    self.repository = repository
+  }
+  
+  
+  // MARK: - Public Methods
+  
+  public func execute(
+    term: String,
+    limit: Int,
+    page: Int
+  ) async throws -> [Movie] {
+    let movies = try await repository.fetchMovie(limit: limit, page: page, term: term, genre: nil, sortBy: nil)
+    return movies
+  }
+}
